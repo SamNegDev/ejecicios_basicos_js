498214ea7999f9e6a41266882e5cd31d4f9fd7ec
SamNegDev <sam.negrin@gmail.com>
2025-06-24 10:53:49 +0100

    Create ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,20 @@
+// Apartado 1.1
+let miSuperHeroeFavorito = "Hulk";
+
+// Apartado 1.2
+let numeroFavorito = 50;
+
+//Apartado 1.3
+let altura = 5;
+let longitud = 10;
+
+//Apartado 1.4
+let suma = altura + longitud;
+
+//Apartado 1.5
+console.log(miSuperHeroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
+
