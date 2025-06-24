566a78d53d70605bd7ddff97cfbc81f62421abe1
SamNegDev <sam.negrin@gmail.com>
2025-06-24 11:22:44 +0100

    Ejercicio 4

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,7 +1,23 @@
-const sable1 = { nombre: "Shoto de Yoda", precio: 1500 };
-const sable2 = { nombre: "Sable de Darth Vader", precio: 2000 };
+//Apartado 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 
-//Obtén el precio total de dos sables de luz: "Shoto de Yoda" y "sable de Darth Vader"
+console.log(10 * 5);
 
-const total = sable1.precio * 2 + sable2.precio * 2;
-console.log("El precio total de 2 sables luz de cada modelo es de : " + total)
+//3.2 Divide 10 por 2 y muestra el resultado en un console.
+console.log(10 / 2);
+
+//3.3 Muestra mediante un console el resto de dividir 15 por 9.
+console.log(15 % 9)
+/*3.4 Usa el correcto operador de asignación que resultará en o = 15,
+    teniendo dos variables p = 10 y j = 5. */
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
+/*3.5 Usa el correcto operador de asignación que resultará en i = 50,
+    teniendo dos variables c = 10 y m = 5. */
+let c = 10;
+let m = 5;
+
+let i = c * m;
+console.log(i); 
--- ejercicio4.js
+++ ejercicio4.js
@@ -1,15 +1,20 @@
-let precioBaseGlobal = 10000;
+//Dado el siguiente array:
+const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
+//4.1 - Saca a "Tendo" por consola atacando su posición.
+console.log(aldeanos[3]);
+//4.2 - Coloca en el último lugar de este array a "Cervasio".
+aldeanos.push("Cervasio");
 
-// Cambia el precio base en esta linea modificando la variable
-precioBaseGlobal = 25000
+//4.3 - Cambia el primer elemento de este array por "Bambina".
+aldeanos.unshift("Bambina");
 
-const nave1 = { nombre: "Ala-X", precioBase: 50000, precioFinal: 60000 };
-const nave2 = { nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000 };
+//4.4 - Dale la vuelta a este array.
+aldeanos.reverse();
 
-// Tu código para actualizar el precio final de cada nave aquí...
-nave1.precioFinal = nave1.precioBase + precioBaseGlobal
-nave2.precioFinal = nave2.precioBase + precioBaseGlobal
+//4.5 - Cambia a "Narciso" por "Canela" haciendo uso de un método de array.
 
-console.log("Precios actualizados correctamente!");
-console.log("Precio Final Ala-X: " + nave1.precioFinal);
-console.log("Precio final Halcón Milenario: " + nave2.precioFinal);
+aldeanos.splice(4, 1, "Canela")
+
+//4.6 - Imprime por consola el último elemento de este array sin atacar a la posición explicitamente
+// Pista: puedes usar el método length con algo más para ello.
+console.log(aldeanos[aldeanos.length - 1]);
